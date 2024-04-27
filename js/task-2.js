309053b2aee797cc70a927d9ba64c2308fc70939
JuliaLuka <lukashova.yuliia26@gmail.com>
2024-04-27 13:10:34 +0200

    Done

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,18 @@
 class Storage {
-constructor ()
+  #items = [];
+
+  constructor(params) {
+    this.#items = params;
+  }
   getItems() {
     return this.#items;
   }
   addItem(newItem) {
     this.#items.push(newItem);
   }
-  removeItem(itemToRemove) {}
+  removeItem(itemToRemove) {
+    this.#items = this.#items.filter((item) => item !== itemToRemove);
+  }
 }
 
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
